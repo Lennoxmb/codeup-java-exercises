4174c79d3caae508594474a360de5c2874ccb19d
Lennox Ballard <noellemb@gmail.com>
2023-04-06 14:42:25 -0500

    start java

---
+++ src/HelloWorld.java
@@ -0,0 +1,34 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int myFavoriteNumber = 9;
+        System.out.println(myFavoriteNumber);
+//        String myString = 3.14159;
+//        System.out.println(myString);
+        float myNumber;
+//        myNumber = 3.14;
+//        myNumber = (float) myNumber;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int class;
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+    }
+}
